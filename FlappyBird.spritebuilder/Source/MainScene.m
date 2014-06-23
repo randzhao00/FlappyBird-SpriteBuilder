d8ce8ac208ce9c24516b1ba18af238c52614c8fc
Randy <Zhao@Randys-MacBook.local>
2014-06-23 16:07:49 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -64,23 +64,23 @@ CCParallaxNode *_parallaxBackground;
     _bushParallaxRatio = ccp(0.9, 1);
     _cloudParallaxRatio = ccp(0.5, 1);
     
-//    for (CCNode *bush in _bushes) {
-//        CGPoint offset = bush.position;
-//        [self removeChild:bush];
-//        [_parallaxBackground addChild:bush z:0 parallaxRatio:_bushParallaxRatio positionOffset:offset];
-//    }
-//    
-//    for (CCNode *cloud in _clouds) {
-//        CGPoint offset = cloud.position;
-//        [self removeChild:cloud];
-//        [_parallaxBackground addChild:cloud z:0 parallaxRatio:_cloudParallaxRatio positionOffset:offset];
-//    }
-//    
-//    for (CCNode *ground in _grounds) {
-//        // set collision txpe
-//        ground.physicsBody.collisionType = @"level";
-//        ground.zOrder = DrawingOrderGround;
-//    }
+    for (CCNode *bush in _bushes) {
+        CGPoint offset = bush.position;
+        [self removeChild:bush];
+        [_parallaxBackground addChild:bush z:0 parallaxRatio:_bushParallaxRatio positionOffset:offset];
+    }
+    
+    for (CCNode *cloud in _clouds) {
+        CGPoint offset = cloud.position;
+        [self removeChild:cloud];
+        [_parallaxBackground addChild:cloud z:0 parallaxRatio:_cloudParallaxRatio positionOffset:offset];
+    }
+    
+    for (CCNode *ground in _grounds) {
+        // set collision txpe
+        ground.physicsBody.collisionType = @"level";
+        ground.zOrder = DrawingOrderGround;
+    }
     
     // set this class as delegate
     physicsNode.collisionDelegate = self;
